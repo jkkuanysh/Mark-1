301cab5bf0ecda7ad6b735cada1b7e4d3ecad1e4
jkkuanysh <ku_zhambyl@kbtu.kz>
2026-04-29 10:25:28 +0500

    Add TSIS1 PhoneBook

---
+++ TSIS_myprojects/TSIS1/connect.py
@@ -0,0 +1,14 @@
+# connect.py
+
+import os
+import sys
+import psycopg2
+
+# Add current folder to Python path so config.py is always found
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from config import load_config
+
+
+def connect():
+    return psycopg2.connect(**load_config())
